1b10afb484fe8ef26989194c11f984b63ec7f4ec
Wolfgang Hotwagner <code@toscom.at>
2018-05-02 18:23:33 +0200

    refactoring

--- lib/cryptorecord/tlsa.rb
+++ lib/cryptorecord/tlsa.rb
@@ -1,21 +1,21 @@
 #--
-# Copyright (C) 2018 Wolfgang Hotwagner <code@feedyourhead.at>       
-#                                                                
-# This file is part of the cryptorecord gem                                            
-# 
-# This mindwave gem is free software; you can redistribute it and/or 
-# modify it under the terms of the GNU General Public License 
+# Copyright (C) 2018 Wolfgang Hotwagner <code@feedyourhead.at>
+#
+# This file is part of the cryptorecord gem
+#
+# This cryptorecord gem is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
 # of the License, or (at your option) any later version.
 # 
-# This mindwave gem is distributed in the hope that it will be useful,
+# This cryptorecord gem is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License          
-# along with this mindwave gem; if not, write to the 
-# Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, 
+# You should have received a copy of the GNU General Public License
+# along with this cryptorecord gem; if not, write to the
+# Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
 # Boston, MA  02110-1301  USA
 #++
 
@@ -25,21 +25,21 @@ module Cryptorecord
 # Cryptorecord::Tlsa-class generates
 # tlsa-dns-records.
 class Tlsa
-# @!attribute [r] selector
-#  stores the selector
-# @!attribute [r] mtype
-#  stores the match-type
-# @!attribute [r] usage
-#  stores the usage
+  # @!attribute [r] selector
+  #  stores the selector
+  # @!attribute [r] mtype
+  #  stores the match-type
+  # @!attribute [r] usage
+  #  stores the usage
   attr_reader :selector, :mtype, :usage
-# @!attribute host
-#  stores the fqdn for the record
-# @!attribute proto
-#  stores the network protocol
-# @!attribute port
-#  stores the network port
-# @!attribute cert
-#  stores the x509 certificate
+  # @!attribute host
+  #  stores the fqdn for the record
+  # @!attribute proto
+  #  stores the network protocol
+  # @!attribute port
+  #  stores the network port
+  # @!attribute cert
+  #  stores the x509 certificate
   attr_accessor :host, :proto, :port, :cert
 	
   def initialize(args={})
