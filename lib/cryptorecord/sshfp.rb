4d076243fd5cb92cd331ec7c81e4c6d6a25cc24b
Wolfgang Hotwagner <code@toscom.at>
2018-05-04 22:42:57 +0200

    docs

--- lib/cryptorecord/sshfp.rb
+++ lib/cryptorecord/sshfp.rb
@@ -28,15 +28,12 @@ module Cryptorecord
   # read from files
   class Sshfp
     # @!attribute [r] cipher
-    #  stores the cipher. ssh-rsa = 1, ssh-dss = 2,
-    #  ecdsa = 3 and ed25519 = 4
-    # @!attribute [r] digest
-    #  stores the digest. sha1 = 1, sha256 = 2
-    # @!attribute [r]  key
-    #  stores the ssh-host-key
+    #   stores the cipher. ssh-rsa = 1, ssh-dss = 2,
+    #   ecdsa = 3 and ed25519 = 4
+    # @!attribute [r] digest stores the digest. sha1 = 1, sha256 = 2
+    # @!attribute [r]  key stores the ssh-host-key
     attr_reader :cipher, :digest, :key
-    # @!attribute host
-    #  stores the fqdn-host
+    # @!attribute host stores the fqdn-host
     attr_accessor :host
 
     # This constructor initializes cipher, key, digest, host and keyfile
